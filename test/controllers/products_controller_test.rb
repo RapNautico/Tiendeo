eec3c2ccf53ac9f5dc451592ee25b90ce44fe421
Emanuel Garcia <foox299@gmail.com>
2023-08-09 17:47:03 -0500

    test for list and detail products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get products_index_url
+  test "render a list of products" do
+    get products_path
     assert_response :success
+    assert_select '.product', 2
+  end
+
+  test "render a detailed product page" do
+    get product_path(products(:ps4))
+    assert_response :success
+    assert_select '.title', 'PS4 Fat'
+    assert_select '.description', 'Ps4 en buen estado'
+    assert_select '.price', '150$'
   end
 end
