1779d6774125d8f3cef11ffb54ae446f0e6acf6e
Emanuel Garcia <foox299@gmail.com>
2023-08-17 16:18:17 -0500

    refactor with turbo streams

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,7 +32,7 @@ class ProductsController < ApplicationController
   def update 
     authorize! product
     if product.update(product_params)
-      notify_all_users
+      product.broadcast
       redirect_to products_path, notice: t('.updated')
     else
       render :edit, status: :unprocessable_entity
@@ -60,12 +60,4 @@ class ProductsController < ApplicationController
     @product ||= Product.find(params[:id])
   end
 
-  def notify_all_users
-    ActionCable.server.broadcast(
-      "product_#{product.id}",
-      {
-        action: "updated"
-      }
-    )
-  end
 end
